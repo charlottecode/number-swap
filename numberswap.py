122638699cb0ca84afb2593b35eabb045883a89b
charlottecode <charlottedesigner2013@gmail.com>
2025-03-05 20:13:16 -0500

    Add files via upload

---
+++ numberswap.py
@@ -0,0 +1,13 @@
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+
+
+print("Before swapping: ",a,b,c)
+temp = a 
+a = b     
+b = c     
+c = temp  
+
+print("After swapping:",temp)
